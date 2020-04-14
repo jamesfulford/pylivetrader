58eb04a3bf6237552aa94bb0fafbed7926906f88
James Fulford <james.fulford@outlook.com>
2020-04-14 18:31:12 -0400

    feat(run): daemon mode

--- run.sh
+++ run.sh
@@ -2,9 +2,17 @@
 
 target_script="${1:-"algorithms/basic-algo.py"}"
 config_file="${2:-"config/.env"}"
+daemon="${3:-"false"}"
+
+optional_args=""
+
+if [[ "false" != "$daemon" ]]; then
+    optional_args="$optional_args -d"
+fi
 
 docker run --rm \
     -v $PWD:/work -w /work \
     --env-file "$config_file" \
+    $optional_args \
     jamespfulford/pylivetrader:latest \
     pylivetrader run -f "$target_script"
